adb330bd1689476b8fbe49874e2ae607012e3871
wbh <946594758@qq.com>
2022-04-08 06:43:23 +0800

    2022年4月8日-泛型-开始

--- 1.java
+++ 1.java
@@ -1,21 +1,30 @@
-// 定义了注解，本身对程序逻辑没有任何影响。我们必须自己编写代码来使用注解。
-void check(Person person) throws IllegalArgumentException, ReflectiveOperationException {
-    // 遍历所有Field:
-    for (Field field : person.getClass().getFields()) {
-        // 获取Field定义的@Range:
-        Range range = field.getAnnotation(Range.class);
-        // 如果@Range存在:
-        if (range != null) {
-            // 获取Field的值:
-            Object value = field.get(person);
-            // 如果值是String:
-            if (value instanceof String) {
-                String s = (String) value;
-                // 判断值是否满足@Range的min/max:
-                if (s.length() < range.min() || s.length() > range.max()) {
-                    throw new IllegalArgumentException("Invalid field: " + field.getName());
-                }
-            }
-        }
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Person[] ps = new Person[] {
+            new Person("Bob", 61),
+            new Person("Alice", 88),
+            new Person("Lily", 75),
+        };
+
+
+    }
+}
+
+
+class Person implements Comparable<Person> {
+    String name;
+    int score;
+    Person(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    
+
+    
+    public String toString() {
+        return this.name + "," + this.score;
     }
 }
