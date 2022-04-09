7b97273d9c268afde233b7c1cb5a054f17ce24a3
wbh <946594758@qq.com>
2022-04-09 08:03:47 +0800

    2022年4月9日-编写泛型-开始

--- 0.java
+++ 0.java
@@ -1,29 +1,15 @@
-import java.util.Arrays;
-
-public class Main {
-    public static void main(String[] args) {
-        Person[] ps = new Person[] {
-            new Person("Bob", 61),
-            new Person("Alice", 88),
-            new Person("Lily", 75),
-        };
-        Arrays.sort(ps);
-        System.out.println(Arrays.toString(ps));
+// 编写同时定义多种类型的泛型
+public class Pair<T, K> {
+    private T first;
+    private K last;
+    public Pair(T first, K last) {
+        this.first = first;
+        this.last = last;
     }
-}
-
-
-class Person implements Comparable<Person> {
-    String name;
-    int score;
-    Person(String name, int score) {
-        this.name = name;
-        this.score = score;
+    public T getFirst() {
+        return first;
     }
-    public int compareTo(Person other) {
-        return this.name.compareTo(other.name);
-    }
-    public String toString() {
-        return this.name + "," + this.score;
+    public K getLast() {
+        return last;
     }
 }
--- 4.java
+++ 4.java
@@ -1,29 +1,15 @@
-import java.util.Arrays;
-
-public class Main {
-    public static void main(String[] args) {
-        Person[] ps = new Person[] {
-            new Person("Bob", 61),
-            new Person("Alice", 88),
-            new Person("Lily", 75),
-        };
-        Arrays.sort(ps);
-        System.out.println(Arrays.toString(ps));
+// 编写同时定义多种类型的泛型
+public class Pair {
+    private String first;
+    private String last;
+    public Pair(String first, String last) {
+        this.first = first;
+        this.last = last;
     }
-}
-
-
-class Person implements Comparable<Person> {
-    String name;
-    int score;
-    Person(String name, int score) {
-        this.name = name;
-        this.score = score;
+    public String getFirst() {
+        return first;
     }
-    public int compareTo(Person other) {
-        return this.name.compareTo(other.name);
-    }
-    public String toString() {
-        return this.name + "," + this.score;
+    public String getLast() {
+        return last;
     }
 }
