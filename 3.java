024f66e80da6db325eded2c9673862cbd0f546bc
wbh <946594758@qq.com>
2022-04-10 07:57:02 +0800

    2022年4月10日-编写泛型-结束

--- 3.java
+++ 3.java
@@ -1,15 +1,15 @@
 // 编写同时定义多种类型的泛型
-public class Pair {
-    private String first;
-    private String last;
-    public Pair(String first, String last) {
+public class Pair<T, K> {
+    private T first;
+    private K last;
+    public Pair(T first, K last) {
         this.first = first;
         this.last = last;
     }
-    public String getFirst() {
-        return first;
+    public T getFirst() {
+        return first; 
     }
-    public String getLast() {
+    public K getLast() {
         return last;
     }
 }
