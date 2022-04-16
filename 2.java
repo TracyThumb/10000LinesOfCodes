ba63e30e6bc45a50bfd6948f7399b3d72a55391c
wbh <946594758@qq.com>
2022-04-16 11:58:05 +0800

    $ git commit -m

--- 2.java
+++ 2.java
@@ -1,15 +1,29 @@
-// 编写同时定义多种类型的泛型
-public class Pair<T, K> {
+// 泛型的extends通配符
+public class Main {
+    public static void main(String[] args) {
+        Pair<Integer> p = new Pair<>(123, 456);
+        int n = add(p);
+        System.out.println(n);
+    }
+
+    static int add(Pair<? extends Number> p) {
+        Number first = p.getFirst();
+        Number last = p.getLast();
+        return first.intValue() + last.intValue();
+    }
+}
+// 编写 Pair 泛型
+class Pair<T> {
     private T first;
-    private K last;
-    public Pair(T first, K last) {
+    private T last;
+    public Pair(T first, T last) {
         this.first = first;
         this.last = last;
     }
     public T getFirst() {
         return first;
     }
-    public K getLast() {
+    public T getLast() {
         return last;
     }
 }
--- 3.java
+++ 3.java
@@ -1,15 +1,28 @@
-// 编写同时定义多种类型的泛型
-public class Pair<T, K> {
-    private T first;
-    private K last;
-    public Pair(T first, K last) {
-        this.first = first;
-        this.last = last;
+// 泛型的extends通配符
+public class Main {
+    public static void main(String[] args) {
+
+
+
     }
-    public T getFirst() {
-        return first; 
-    }
-    public K getLast() {
-        return last;
-    }
+
+
+
+
+
+
 }
+// 编写 Pair 泛型
+
+
+
+
+
+
+
+
+
+
+
+
+
