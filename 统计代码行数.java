a6792ccd035ad944d02bfddf20a4bea78d0a0e8a
wbh <946594758@qq.com>
2022-04-17 09:29:34 +0800

    2022年4月17日 泛型 extends

--- 统计代码行数.java
+++ 统计代码行数.java
@@ -303,3 +303,32 @@ class Person implements Comparable<Person> {
         return this.name + "," + this.score;
     }
 }
+// 泛型的extends通配符
+public class Main {
+    public static void main(String[] args) {
+        Pair<Integer> p = new Pair<>(123, 456);
+        int n = add(p);
+        System.out.println(n);
+    }
+
+    static int add(Pair<? extends Number> p) {
+        Number first = p.getFirst();
+        Number last = p.getLast();
+        return first.intValue() + last.intValue();
+    }
+}
+// 编写 Pair 泛型
+class Pair<T> {
+    private T first;
+    private T last;
+    public Pair(T first, T last) {
+        this.first = first;
+        this.last = last;
+    }
+    public T getFirst() {
+        return first;
+    }
+    public T getLast() {
+        return last;
+    }
+}
