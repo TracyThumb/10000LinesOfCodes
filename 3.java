4705c816e24607a5c539e99907fbb589f58bbfce
wbh <946594758@qq.com>
2022-04-19 06:20:02 +0800

    2022年4月19日，Collections 的 copy 方法，begin

--- 3.java
+++ 3.java
@@ -1,29 +1,3 @@
-// 泛型的extends通配符
-public class Main {
-    public static void main(String[] args) {
-        Pair<Integer> p = new Pair<>(123, 456);
-        int n = add(p);
-        System.out.println(n);
-    }
-
-    static int add(Pair<? extends Number> p) {
-        Number first = p.getFirst();
-        Number last = p.getLast();
-        return first.intValue() + last.intValue();
-    }
-}
-// 编写 Pair 泛型
-class Pair<T> {
-    private T first;
-    private T last;
-    public Pair(T first, T last) {
-        this.first = first;
-        this.last = last;
-    }
-    public T getFirst() {
-        return first;
-    }
-    public T getLast() {
-        return last;
-    }
-}
+// Collections 的 copy 方法
+// src是producer
+// dest是consumer
