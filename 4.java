84e68c798d0caa6cdb7000c5ca9b0dcc73d5c619
wbh <946594758@qq.com>
2022-04-20 06:15:22 +0800

    2022年4月20日-Collections 的 copy 方法-end

--- 4.java
+++ 4.java
@@ -1,3 +1,11 @@
 // Collections 的 copy 方法
 // src是producer
-// dest是consumers
+// dest是consumer
+public class Collections {
+    public static <T> void copy(List<? super T> dest, List<? extends T> src) {
+        for (int i=0; i<src.size(); i++) {
+            T t = src.get(i);
+            dest.add(t);
+        }
+    }
+}
