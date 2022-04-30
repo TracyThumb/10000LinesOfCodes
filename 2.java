f8360fb1bab19ed6dd7a33aef796152f4885a975
wbh <946594758@qq.com>
2022-04-30 12:35:04 +0800

    2022年4月30日-用 map 缓存数据，提升 list 的查找效率-begin

--- 2.java
+++ 2.java
@@ -1,34 +1,57 @@
-// 给 Person 类增加 equals 方法，使得调用 indexOf() 方法返回正常：
-import java.util.List;
-import java.util.Objects;
-
+// 请编写一个根据name查找score的程序，并利用Map充当缓存，以提高查找效率：
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        List<Person> list = List.of(
-                new Person("Xiao", "Ming", 18),
-                new Person("Xiao", "Hong", 25),
-                new Person("Bob", "Smith", 20));
-        boolean exist = list.contains(new Person("Bob", "Smith", 20));
-        System.out.println(exist ? "测试成功!" : "测试失败!");
+        List<Student> list = List.of(
+            new Student("Bob", 78),
+            new Student("Alice", 85),
+            new Student("Brush", 66),
+            new Student("Newton", 99));
+        var holder = new Students(list);
+        System.out.println(holder.getScore("Bob") == 78 ? "测试成功!" : "测试失败!");
+        System.out.println(holder.getScore("Alice") == 85 ? "测试成功!" : "测试失败!");
+        System.out.println(holder.getScore("Tom") == -1 ? "测试成功!" : "测试失败!");
     }
 }
 
-class Person {
-    String firstName;
-    String lastName;
-    int age;
-
-    public Person(String firstName, String lastName, int age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
+class Students {
+    List<Student> list;
+    Map<String, Integer> cache;
+    Students(List<Student> list) {
+        this.list = list;
+        cache = new HashMap<>();
     }
-    // 定义 equals 方法
-    public boolean equals(Object o) {
-        if (o instanceof Person) {
-            Person p = (Person) o;
-            return Objects.equals(this.firstName, p.firstName) && Objects.equals(this.lastName, p.lastName) && this.age == p.age;
+    /**
+     * 根据name查找score，找到返回score，未找到返回-1
+     */
+    int getScore(String name) {
+        // 先在Map中查找:
+        Integer score = this.cache.get(name);
+        if (score == null) {
+            score = this.findInList(name);
+            if (score != null) {
+                this.cache.put(name, score);
+            }
         }
-        return false;
+        return score == null ? -1 : score.intValue();
+    }
+    // 原始方法
+    Integer findInList(String name) {
+        for (var ss : this.list) {
+            if (ss.name.equals(name)) {
+                return ss.score;
+            }
+        }
+        return null;
+    }
+}
+
+class Student {
+    String name;
+    int score;
+
+    Student(String name, int score) {
+        this.name = name;
+        this.score = score;
     }
 }
--- 3.java
+++ 3.java
@@ -1,34 +1,57 @@
-// 给 Person 类增加 equals 方法，使得调用 indexOf() 方法返回正常：
-import java.util.List;
-import java.util.Objects;
-
+// 请编写一个根据name查找score的程序，并利用Map充当缓存，以提高查找效率：
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        List<Person> list = List.of(
-                new Person("Xiao", "Ming", 18),
-                new Person("Xiao", "Hong", 25),
-                new Person("Bob", "Smith", 20));
-        boolean exist = list.contains(new Person("Bob", "Smith", 20));
-        System.out.println(exist ? "测试成功!" : "测试失败!");
+        List<Student> list = List.of(
+            new Student("Bob", 78),
+            new Student("Alice", 85),
+            new Student("Brush", 66),
+            new Student("Newton", 99));
+        var holder = new Students(list);
+        System.out.println(holder.getScore("Bob") == 78 ? "测试成功!" : "测试失败!");
+        System.out.println(holder.getScore("Alice") == 85 ? "测试成功!" : "测试失败!");
+        System.out.println(holder.getScore("Tom") == -1 ? "测试成功!" : "测试失败!");
     }
 }
 
-class Person {
-    String firstName;
-    String lastName;
-    int age;
+class Students {
 
-    public Person(String firstName, String lastName, int age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-    }
-    // 定义 equals 方法
-    public boolean equals(Object o) {
-        if (o instanceof Person) {
-            Person p = (Person) o;
-            return Objects.equals(this.firstName, p.firstName) && Objects.equals(this.lastName, p.lastName) && this.age == p.age;
-        }
-        return false;
+    
+
+
+
+    
+    /**
+     * 根据name查找score，找到返回score，未找到返回-1
+     */
+
+
+
+
+
+
+
+
+
+
+    
+    // 原始方法
+
+
+
+
+
+
+
+
+}
+
+class Student {
+    String name;
+    int score;
+
+    Student(String name, int score) {
+        this.name = name;
+        this.score = score;
     }
 }
