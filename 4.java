c1b9b96f594904d2f0b6c267bbc7d692330de1fe
wbh <946594758@qq.com>
2022-05-02 09:32:03 +0800

    2022年5月2日-TreeMap,定义了Student类，并用分数score进行排序，高分在前-end

--- 4.java
+++ 4.java
@@ -2,13 +2,20 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-
+        Map<Student, Integer> map = new TreeMap<>(new Comparator<Student>() {
+            public int compare(Student p1, Student p2) {
+                if (p1.score == p2.score) {
+                    return 0;
+                }
+                return p1.score > p2.score ? -1 : 1;
+            }
+        });
         map.put(new Student("Tom", 77), 1);
         map.put(new Student("Bob", 66), 2);
         map.put(new Student("Lily", 99), 3);
-
-
-
+        for (Student key : map.keySet()) {
+            System.out.println(key);
+        }
         System.out.println(map.get(new Student("Bob", 66))); // null?
     }
 }
@@ -20,8 +27,8 @@ class Student {
         this.name = name;
         this.score = score;
     }
-    
-
-
+    public String toString() {
+        return String.format("{%s: score=%d}", name, score);
+    }
 }
 
