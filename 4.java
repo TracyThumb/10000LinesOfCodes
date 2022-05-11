f2703334fe124b7d83fd81b70506e0a777ce0fdc
wbh <946594758@qq.com>
2022-05-12 06:52:48 +0800

    2022年5月12日-通过 Map 去重消息-begin

--- 4.java
+++ 4.java
@@ -1,20 +1,33 @@
-// 实际上，HashSet仅仅是对HashMap的一个简单封装，它的核心代码如下
-public class HashSet<E> implements Set<E> {
-    // 持有一个HashMap:
-    private HashMap<E, Object> map = new HashMap<>();
-
-    // 放入HashMap的value:
-    private static final Object PRESENT = new Object();
-
-    public boolean add(E e) {
-        return map.put(e, PRESENT) == null;
+// 在聊天软件中，发送方发送消息时，遇到网络超时后就会自动重发，因此，接收方可能会收到重复的消息，在显示给用户看的时候，需要首先去重。
+// 请练习使用Set去除重复的消息：
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        List<Message> received = List.of(
+            new Message(1, "Hello!"),
+            new Message(2, "发工资了吗？"),
+            new Message(2, "发工资了吗？"),
+            new Message(3, "去哪吃饭？"),
+            new Message(3, "去哪吃饭？"),
+            new Message(4, "Bye")
+        );
+        List<Message> displayMessages = process(received);
+        for (Message message : displayMessages) {
+            System.out.println(message.text);
+        }
     }
 
-    public boolean contains(Object o) {
-        return map.containsKey(o);
+    static List<Message> process(List<Message> received) {
+        // TODO: 按sequence去除重复消息
+        
     }
+}
 
-    public boolean remove(Object o) {
-        return map.remove(o) == PRESENT;
+class Message {
+    public final int sequence;
+    public final String text;
+    public Message(int sequence, String text) {
+        this.sequence = sequence;
+        this.text = text;
     }
 }
