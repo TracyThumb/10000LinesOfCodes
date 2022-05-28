3c45760babb2333cd59c514d6b0a7cb14bcbedc3
wbh <946594758@qq.com>
2022-05-28 11:57:04 +0800

    2022年5月28日-过滤文件-begin

--- 0.java
+++ 0.java
@@ -1,47 +1,27 @@
-// 一个简单的Iterator示例如下，它总是以倒序遍历集合
-import java.util.*;
-
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        ReverseList<String> rlist = new ReverseList<>();
-        rlist.add("Apple");
-        rlist.add("Orange");
-        rlist.add("Pear");
-        for (String s : rlist) {
-            System.out.println(s);
-        }
-    }
-}
-
-class ReverseList<T> implements Iterable<T> {
-
-    private List<T> list = new ArrayList<>();
-
-    public void add(T t) {
-        list.add(t);
+    public static void main(String[] args) throws IOException {
+        File f = new File("C:\\Windows");
+        // 列出所有文件和子目录
+        File[] fs1 = f.listFiles();
+        printFiles(fs1);
+        // 仅列出.exe文件
+        File[] fs2 = f.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                // 返回true表示接受该文件
+                return name.endsWith(".exe");
+            }
+        });
+        printFiles(fs2);
     }
 
-    @Override
-    public Iterator<T> iterator() {
-        return new ReverseIterator(list.size());
-    }
-
-    class ReverseIterator implements Iterator<T> {
-        int index;
-
-        ReverseIterator(int index) {
-            this.index = index;
+    static void printFiles(File[] files) {
+        System.out.println("==========");
+        if (files != null) {
+            for (File f : files) {
+                System.out.println(f);
+            }
         }
-
-        @Override
-        public boolean hasNext() {
-            return index > 0;
-        }
-
-        @Override
-        public T next() {
-            index--;
-            return ReverseList.this.list.get(index);
-        }
+        System.out.println("==========");
     }
 }
--- 3.java
+++ 3.java
@@ -1,47 +1,29 @@
-// 一个简单的Iterator示例如下，它总是以倒序遍历集合
-import java.util.*;
-
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        ReverseList<String> rlist = new ReverseList<>();
-        rlist.add("Apple");
-        rlist.add("Orange");
-        rlist.add("Pear");
-        for (String s : rlist) {
-            System.out.println(s);
-        }
-    }
-}
+    public static void main(String[] args) throws IOException {
 
-class ReverseList<T> implements Iterable<T> {
+        // 列出所有文件和子目录
 
-    private List<T> list = new ArrayList<>();
+        printFiles(fs1);
+        // 仅列出.exe文件
 
-    public void add(T t) {
-        list.add(t);
+        
+                // 返回true表示接受该文件
+
+                
+
+
+        printFiles(fs2);
     }
 
-    @Override
-    public Iterator<T> iterator() {
-        return new ReverseIterator(list.size());
-    }
 
-    class ReverseIterator implements Iterator<T> {
-        int index;
+    
 
-        ReverseIterator(int index) {
-            this.index = index;
-        }
 
-        @Override
-        public boolean hasNext() {
-            return index > 0;
-        }
 
-        @Override
-        public T next() {
-            index--;
-            return ReverseList.this.list.get(index);
-        }
-    }
+
+
+
+
+    
 }
