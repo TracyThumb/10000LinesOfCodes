13b6377ca72ec001b55629da1491cb3a9957fe8a
wbh <946594758@qq.com>
2022-06-14 06:32:04 +0800

    过滤文件-end

--- 统计代码行数.java
+++ 统计代码行数.java
@@ -652,6 +652,30 @@ class ReverseList<T> implements Iterable<T> {
         }
     }
 }
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File f = new File("C:\\Windows");
+        // 列出所有文件和子目录
+        File[] fs1 = f.listFiles();
+        printFiles(fs1);
+        // 仅列出.exe文件
+        File[] fs2 = f.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                // 返回true表示接受该文件
+                return name.endsWith(".exe");
+            }
+        });
+        printFiles(fs2);
+    }
 
-
-
+    static void printFiles(File[] files) {
+        System.out.println("==========");
+        if (files != null) {
+            for (File f : files) {
+                System.out.println(f);
+            }
+        }
+        System.out.println("==========");
+    }
+}
