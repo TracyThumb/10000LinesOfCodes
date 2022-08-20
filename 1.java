0aeedf27520ea031f3d47adbc1a721cf425218cb
wbh <946594758@qq.com>
2022-08-20 14:46:11 +0800

    模拟输入流

--- 1.java
+++ 1.java
@@ -1,27 +1,18 @@
-import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
-        File f = new File("C:\\Windows");
-        // 列出所有文件和子目录
-        File[] fs1 = f.listFiles();
-        printFiles(fs1);
-        // 仅列出.exe文件
-        File[] fs2 = f.listFiles(new FilenameFilter() {
-            public boolean accept(File dir, String name) {
-                // 返回true表示接受该文件
-                return name.endsWith(".exe");
-            }
-        });
-        printFiles(fs2);
+        byte[] data = { 72, 101, 108, 108, 111, 33 };
+        try (InputStream input = new ByteArrayInputStream(data)) {
+            String s = readAsString(input);
+            System.out.println(s);
+        }
     }
 
-    static void printFiles(File[] files) {
-        System.out.println("==========");
-        if (files != null) {
-            for (File f : files) {
-                System.out.println(f);
-            }
+    public static String readAsString(InputStream input) throws IOException {
+        int n;
+        StringBuilder sb = new StringBuilder();
+        while ((n = input.read()) != -1) {
+            sb.append((char) n);
         }
-        System.out.println("==========");
+        return sb.toString();
     }
 }
--- 统计代码行数.java
+++ 统计代码行数.java
@@ -679,3 +679,24 @@ public class Main {
         System.out.println("==========");
     }
 }
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        byte[] data = { 72, 101, 108, 108, 111, 33 };
+        try (InputStream input = new ByteArrayInputStream(data)) {
+            String s = readAsString(input);
+            System.out.println(s);
+        }
+    }
+
+    public static String readAsString(InputStream input) throws IOException {
+        int n;
+        StringBuilder sb = new StringBuilder();
+        while ((n = input.read()) != -1) {
+            sb.append((char) n);
+        }
+        return sb.toString();
+    }
+}
