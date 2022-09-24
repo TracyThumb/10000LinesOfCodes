1352b21c7509ffbf3edce9fd4706dc18113c823e
wbh <946594758@qq.com>
2022-09-25 07:12:45 +0800

    filter model，装饰者模式

--- 0.java
+++ 0.java
@@ -1,18 +1,41 @@
+import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
-        byte[] data = { 72, 101, 108, 108, 111, 33 };
-        try (InputStream input = new ByteArrayInputStream(data)) {
-            String s = readAsString(input);
-            System.out.println(s);
+        byte[] data = "hello, world!".getBytes("UTF-8");
+        try (CountInputStream input = new CountInputStream(new ByteArrayInputStream(data))) {
+            int n;
+            while ((n = input.read()) != -1) {
+                System.out.println((char)n);
+            }
+            System.out.println("Total read " + input.getBytesRead() + " bytes");
         }
     }
+}
 
-    public static String readAsString(InputStream input) throws IOException {
-        int n;
-        StringBuilder sb = new StringBuilder();
-        while ((n = input.read()) != -1) {
-            sb.append((char) n);
+class CountInputStream extends FilterInputStream {
+    private int count = 0;
+
+    CountInputStream(InputStream in) {
+        super(in);
+    }
+
+    public int getBytesRead() {
+        return this.count;
+    }
+
+    public int read() throws IOException {
+        int n = in.read();
+        if (n != -1) {
+            this.count ++;
         }
-        return sb.toString();
+        return n;
+    }
+
+    public int read(byte[] b, int off, int len) throws IOException {
+        int n = in.read(b, off, len);
+        if (n != -1) {
+            this.count += n;
+        }
+        return n;
     }
 }
--- 1.java
+++ 1.java
@@ -1,18 +1,1 @@
-public class Main {
-    public static void main(String[] args) throws IOException {
-        byte[] data = { 72, 101, 108, 108, 111, 33 };
-        try (InputStream input = new ByteArrayInputStream(data)) {
-            String s = readAsString(input);
-            System.out.println(s);
-        }
-    }
-
-    public static String readAsString(InputStream input) throws IOException {
-        int n;
-        StringBuilder sb = new StringBuilder();
-        while ((n = input.read()) != -1) {
-            sb.append((char) n);
-        }
-        return sb.toString();
-    }
-}
+import java.io.*;
